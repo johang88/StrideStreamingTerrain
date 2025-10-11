7459797997fc016b239b157b248c5f9e84cc613b
Johan Gustafsson <johan@tinyrocket.se>
2025-10-11 19:58:57 +0200

    Remvoe TR.Stride.Atmosphere code

--- TR.Stride.Atmosphere/AtmosphereShadowKeys.cs
+++
@@ -1,7 +0,0 @@
-﻿using Stride.Rendering;
-
-namespace TR.Stride.Atmosphere;
-public static class AtmosphereShadowKeys
-{
-    public static readonly PermutationParameterKey<string> ShadowFunction = ParameterKeys.NewPermutation("ShadowFunction");
-}
