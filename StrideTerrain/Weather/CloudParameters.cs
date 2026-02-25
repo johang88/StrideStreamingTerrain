1ab661b95cbb63e58702a73d6a08df9b4cf02f85
Johan Gustafsson <johan@tinyrocket.se>
2026-02-25 15:29:04 +0100

    slightly better clouds

--- StrideTerrain/Weather/CloudParameters.cs
+++ StrideTerrain/Weather/CloudParameters.cs
@@ -10,11 +10,13 @@ public struct CloudParameters
     [DataMember] public float Scale;
     [DataMember] public float Speed;
     [DataMember] public float Cloudiness;
+    [DataMember] public float CirrusAmount;
 
     public CloudParameters()
     {
         Scale = 0.001f;
         Speed = 0.02f;
         Cloudiness = 0.3f;
+        CirrusAmount = 0.5f;
     }
 }
